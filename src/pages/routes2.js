13c9610dafcae118498b5c11657e896effd3e361
Diogo Felipe <diogo.felipe@outlook.com>
2021-06-17 21:26:24 -0300

    it is done

--- src/pages/routes2.js
+++ src/pages/routes2.js
@@ -1,5 +1,6 @@
 export default [
     { path: 'cadastro', name: 'cadastro', component: () => import('./Cadastro.vue') },
     { path: 'login', name: 'login', component: () => import('./auth/pages/Login.vue') },
+    { path: '', name: 'login', component: () => import('./auth/pages/Login.vue') },
     { path: 'esqueceu-a-senha', name: 'esqueceu-a-senha', component: () => import('./EsqueceuSenha.vue') },
 ]
